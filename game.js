a8ea329b698085df156898a43d64698056e0e605
Italo Lelis <italohdc@gmail.com>
2018-05-28 17:25:30 -0300

    Snake inside module

--- game.js
+++ game.js
@@ -1,12 +1,5 @@
-var snake = function () {
-  function setup () {
-    document.body.style.backgroundColor='rgba(200,200,200,0.8)';
-    canv = document.getElementById('gc');
-    ctx = canv.getContext('2d');
-    document.addEventListener('keydown', keyPush);
-    setInterval(game,1000/15);
-  }
-
+var snake = (function () {
+  
   var velocityX = 0;
   var velocityY = 0;
 
@@ -21,42 +14,20 @@ var snake = function () {
 
   var trail = [];
   var tail = 5;
+  
+  function setup () {
+    document.body.style.backgroundColor='rgba(225,225,225,0.2)';
+    canv = document.getElementById('gc');
+    ctx = canv.getContext('2d');
+    document.addEventListener('keydown', keyPush);
+    
+    game.reset();
+    setInterval(game.loop,1000/15);
+  }
 
-  function game () {
-    playerX += velocityX;
-    playerY += velocityY;
-    
-    if(playerX < 0) playerX = tileCount-1;
-    if(playerX > tileCount-1) playerX = 0;
-    if(playerY < 0) playerY = tileCount-1;
-    if(playerY > tileCount-1) playerY = 0;
-    
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0,0,canv.width,canv.height);
-    
-    for(var i=0; i<trail.length; i++) {
-      ctx.fillStyle = 'lime';
-      ctx.fillRect(trail[i].x * gridSize+1, trail[i].y * gridSize+1, gridSize-2, gridSize-2);
-      
-      if (trail[i].x == playerX && trail[i].y == playerY) reset();
-    }
-    
-    if (playerX == fruitX && playerY == fruitY) {
-      tail++;
-      fruitX = Math.floor(Math.random()*tileCount);
-      fruitY = Math.floor(Math.random()*tileCount);
-    }
-    trail.push({x:playerX, y:playerY});
-    while(trail.length > tail) trail.shift();
-    
-    ctx.fillStyle = 'red';
-    ctx.fillRect(fruitX * gridSize+1, fruitY * gridSize+1, gridSize-2, gridSize-2);
-    
-    console.log('====================');
-    console.log('x:' + playerX + ', y:' + playerY);
-    console.log('tail:' + tail + ', trail.length:' + trail.length);
+  var game = {
 
-    this.reset = function () {
+    reset: function () {
       tail = 5;
       velocityX = 0;
       velocityY = 0;
@@ -64,41 +35,99 @@ var snake = function () {
       playerY = 10;
 
       trail = [];
-      trail.push({ x: playerX, y: playerY });      
-    }
+      trail.push({ x: playerX, y: playerY });
+    },
 
-    return this;
+    action: {
+      up: function () {
+        if (!(trail[0].y+1 == trail[1].y)){
+          velocityX = 0;
+          velocityY = -1;
+        }
+      },
+      down: function () {
+        if (!(trail[0].y-1 == trail[1].y)){
+          velocityX = 0;
+          velocityY = 1;
+        }
+      },
+      left: function () {
+        if (!(trail[0].x+1 == trail[1].x)) {
+          velocityX = -1;
+          velocityY = 0;
+        }
+      },
+      right: function () {
+        if (!(trail[0].x-1 == trail[1].x)) {
+          velocityX = 1;
+          velocityY = 0;
+        }
+      }
+    },
+
+    log: function () {
+      console.log('====================');
+      console.log('x:' + playerX + ', y:' + playerY);
+      console.log('tail:' + tail + ', trail.length:' + trail.length);
+    },
+
+    loop: function () {
+      playerX += velocityX;
+      playerY += velocityY;
+      
+      if(playerX < 0) playerX = tileCount-1;
+      if(playerX > tileCount-1) playerX = 0;
+      if(playerY < 0) playerY = tileCount-1;
+      if(playerY > tileCount-1) playerY = 0;
+      
+      ctx.fillStyle = 'black';
+      ctx.fillRect(0,0,canv.width,canv.height);
+      
+      for(var i=0; i<trail.length; i++) {
+        ctx.fillStyle = 'lime';
+        ctx.fillRect(trail[i].x * gridSize+1, trail[i].y * gridSize+1, gridSize-2, gridSize-2);
+        
+        if (trail[i].x == playerX && trail[i].y == playerY) game.reset();
+      }
+      
+      if (playerX == fruitX && playerY == fruitY) {
+        tail++;
+        fruitX = Math.floor(Math.random()*tileCount);
+        fruitY = Math.floor(Math.random()*tileCount);
+      }
+      trail.push({x:playerX, y:playerY});
+      while(trail.length > tail) trail.shift();
+      
+      ctx.fillStyle = 'red';
+      ctx.fillRect(fruitX * gridSize+1, fruitY * gridSize+1, gridSize-2, gridSize-2);
+      
+      game.log();
+    }
   }
 
   function keyPush (evt) {
     switch(evt.keyCode) {
       case 37: //left
-        velocityX=-1;
-        velocityY=0;
+        game.action.left();
         break;
 
       case 38: //up
-        velocityX=0;
-        velocityY=-1;
+        game.action.up();
         break;
-
-      case 39: //right
-        velocityX=1;
-        velocityY=0;
+        
+        case 39: //right
+        game.action.right();
         break;
-
-      case 40: //down
-        velocityX=0;
-        velocityY=1;
+        
+        case 40: //down
+        game.action.down();
         break;
 
       case 32:
       case 27:
-        game().reset();
+        game.reset();
     }
   }
 
   window.onload = setup;
-}
-
-snake();
+})();
