95d46086087db6836f3aa7cdac1fae039d57feb3
Italo Lelis <italohdc@gmail.com>
2018-05-30 00:33:28 -0300

    Cleaning code a bit

--- game.js
+++ game.js
@@ -24,7 +24,6 @@ var snake = (function () {
     canv = document.getElementById('gc');
     ctx = canv.getContext('2d');
     document.addEventListener('keydown', keyPush);
-    // document.removeEventListener('keydown', keyPush);
     
     game.reset();
     setInterval(game.loop,1000/FPS);
@@ -33,7 +32,6 @@ var snake = (function () {
   var game = {
 
     reset: function () {
-      console.debug('========\nNEW GAME\n========');
       tail = INITIAL_TAIL;
       velocity.x = 0;
       velocity.y = 0;
@@ -53,36 +51,32 @@ var snake = (function () {
         if (lastAction != ActionEnum.down){
           velocity.x = 0;
           velocity.y = -1;
-          // lastAction = ActionEnum.up;
         }
       },
       down: function () {
         if (lastAction != ActionEnum.up){
           velocity.x = 0;
           velocity.y = 1;
-          // lastAction = ActionEnum.down;
         }
       },
       left: function () {
         if (lastAction != ActionEnum.right){
           velocity.x = -1;
           velocity.y = 0;
-          // lastAction = ActionEnum.left;
         }
       },
       right: function () {
         if (lastAction != ActionEnum.left){
           velocity.x = 1;
           velocity.y = 0;
-          // lastAction = ActionEnum.right;
         }
       }
     },
 
     log: function () {
-      console.debug('====================');
-      console.debug('x:' + player.x + ', y:' + player.y);
-      console.debug('tail:' + tail + ', trail.length:' + trail.length);
+      console.log('====================');
+      console.log('x:' + player.x + ', y:' + player.y);
+      console.log('tail:' + tail + ', trail.length:' + trail.length);
     },
 
     loop: function () {
@@ -110,7 +104,7 @@ var snake = (function () {
 
       DontHitWall();
 
-      game.log();
+      // game.log();
 
       trail.push({x:player.x, y:player.y});
       while(trail.length > tail) trail.shift();
@@ -122,7 +116,7 @@ var snake = (function () {
       for(var i=0; i<trail.length-1; i++) {
         ctx.fillRect(trail[i].x * gridSize+1, trail[i].y * gridSize+1, gridSize-2, gridSize-2);
         
-        console.log(i + ' => player:' + player.x, player.y + ', trail:' + trail[i].x, trail[i].y);
+        // console.debug(i + ' => player:' + player.x, player.y + ', trail:' + trail[i].x, trail[i].y);
         if (!(velocity.x == 0 && velocity.y == 0) && trail[i].x == player.x && trail[i].y == player.y){
           game.reset();
         }
